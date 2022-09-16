afbd89dafefa96f745ef570f1bf780eb904caf94
Mohammad Belkadi <uncleruckus@mohammad.myfiosgateway.com>
2022-09-15 20:57:11 -0400

    temp conversion

--- temp_conversion.py
+++ temp_conversion.py
@@ -7,9 +7,8 @@ def convert_100_to_celsius():
     # Print 'float' if it is a float or 'int' if it is an int
     # How do you know? Write a comment below your code explaining how you know which it is
     celsius_100 = (100 - 32) * 5 / 9
-    print('celsius_100')
     print(celsius_100)
-    print(float)
+    print('float')
     # it is a float because 37.7 cannot be an interger due to it being a decimal.
 convert_100_to_celsius()
 
@@ -17,7 +16,6 @@ def convert_0_to_celsius():
     # Convert a temperature of 0 degrees fahrenheit to celsius
     # Save this to a variable called celsius_0, and use print() to print out the value
     celsius_0 = (0 - 32) * 5 / 9
-    print('celsius_0')
     print(celsius_0)
 convert_0_to_celsius()
 
@@ -41,7 +39,6 @@ convert_5_to_fahrenheit()
 def hotter_temp():
     # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
     # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
-   hotter_temp = (85.1 - 32) * 5 / 9
-   print('30.2')
+   print('30.2 degrees celsius')
 hotter_temp()
 
